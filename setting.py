02b7dd24cab297162a2c4d7e85b171b82ab43057
naubmw <104247018+naubmw@users.noreply.github.com>
2024-01-31 23:52:20 +0800

    1

---
+++ setting.py
@@ -0,0 +1,7 @@
+# Замените 'YourTelegramBotToken' на ваш токен бота в Telegram
+bot_token = '6838274568:AAE-PSU4Dojw08HZrM2EhWrd5-ZfdNBOfgA'
+bot_chatID = '-1001800465682'
+
+# Замените 'YourVKGroupID' на ID вашей группы Вконтакте
+vk_group_id = '217414089'
+vk_access_token = '52782737527827375278273707516f3d9a552785278273737c201f6d60605b7bfb0e913'
